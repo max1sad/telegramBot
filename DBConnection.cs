f919ad895110d0d34495aff9406f6750145a38d3
79963 <spartach.chempion741>
2022-07-26 20:37:09 +0300

    подключение к БД, сформирвоана логика вывода сообщений в телеграмм от бота, для подачи заявки, отслеживание на какую кнопку нажато

---
+++ DBConnection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace FootballTelegramBot
+{
+    class DBConnection
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=DESKTOP-2MK3795;Initial Catalog=dbfootbal;Integrated Security=True");
+        public void OpenConnect()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                Console.WriteLine("Соединение");
+                sqlConnection.Open();
+            }
+        }
+        public void CloseConnet()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+        public void SqlRead(String sqlstr)
+        {
+            try
+            {
+                using (SqlCommand sqlCommand = new SqlCommand(sqlstr,sqlConnection))
+                {
+                    sqlConnection.Open();
+                    Console.WriteLine(sqlCommand.CommandText);                   
+                    using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            Console.WriteLine(sqlDataReader.GetString(0));
+                        }
+                    }
+                }
+                
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            
+                
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,13 +12,14 @@ namespace FootballTelegramBot
     class Program
     {
         static ITelegramBotClient bot = new TelegramBotClient("5444116745:AAEiHTg9bpEQLHA7sEGj_c7SpCTg6AXMVPY");
-        
+        static bool labelClick = false;
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient,Update update, CancellationToken cancellationToken)
         {   string actionChoice = "";
-            var message = update.Message;
-            //await botClient.SendTextMessageAsync(message.Chat.Id, "П");
-            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
             
+            var message = update.Message;
+            //результат ответа от телеграмма выводится
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+            //бот случает чат телеграмма на новые сообщения
             if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
             {
                 
@@ -26,85 +27,87 @@ namespace FootballTelegramBot
                 //var message = update.Message;
                 //сообщение которое приходит от бота 
                 //await botClient.SendTextMessageAsync(message.Chat,message.Text);
-                if (message.Text != null)
+                if (message.Text == "/start")
                 {
+                     labelClick = true;
                     //сообщение от бота с выделением конкретного сообщения, кто отправил
                     //await botClient.SendTextMessageAsync(message.Chat.Id, message.Text,replyToMessageId: message.MessageId);
                     //создание кнопки в телеграмме
-                   
-                    await botClient.SendTextMessageAsync(message.Chat.Id,message.Text,replyMarkup: buttonTest());
-                    
-                    //Console.WriteLine(message.Contact.PhoneNumber);
+                    await botClient.SendTextMessageAsync(message.Chat.Id,"Привет человек");
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Выбири действия", replyMarkup: buttonTest());
                     return;
-                   
                 }
-                await botClient.SendTextMessageAsync(message.Chat, "Привет-привет");
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Для начала работы напиши \"/start\"");
             }
-           //проверка на какую кнопку нажали
-            CallbackQuery callback = update.CallbackQuery;
-            switch (callback.Data)
+            Console.WriteLine(labelClick);
+            //проверка на какую кнопку нажали
+            if (labelClick == true)
             {
-                case "myCommand1":
-                    Console.WriteLine("кнопка нажата");
-                    actionChoice = "myCommand1";
-                    break;
-                case "myCommand2":
-                    Console.WriteLine("кнопка нажата1");
-                    actionChoice = "myCommand2";
-                    break;
-                case "myCommand3":
-                    Console.WriteLine("кнопка нажата2");
-                    actionChoice = "myCommand3";
-                    break;
-            }
-            //проверка было ли какое то действие связнное с CallbackQuery, проверка на какую кнопку нажато
-            var updateCallbackQuery = update.CallbackQuery;
-            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.CallbackQuery)
-            {
-                if (actionChoice == "myCommand1")
+                //Console.WriteLine("Не заходит в условие");
+                labelClick = false;
+                CallbackQuery callback = update.CallbackQuery;
+                //проверка было ли какое то действие связнное с CallbackQuery, проверка на какую кнопку нажато- 
+                var updateCallbackQuery = update.CallbackQuery;
+                switch (callback.Data)
                 {
-                    await botClient.SendTextMessageAsync(updateCallbackQuery.Message.Chat.Id,actionChoice);
+                    case "tourneyTable":
+                        Console.WriteLine("кнопка нажата");
+                        actionChoice = "tourneyTable";
+                        break;
+                    case "gameSchedule":
+                        Console.WriteLine("кнопка нажата1");
+                        actionChoice = "gameSchedule";
+                        break;
+                    case "apply":
+                        Console.WriteLine("кнопка нажата2");
+                        actionChoice = "apply";
+                        if (update.Type == Telegram.Bot.Types.Enums.UpdateType.CallbackQuery & actionChoice == "apply")
+                        {
+                            await bot.SendTextMessageAsync(updateCallbackQuery.Message.Chat.Id, "Выбери лигу, указав номер");
+                            //вывод списка лиг из бд
+                            Console.WriteLine(actionChoice);
+                        }
+                        break;
                 }
+
+                
             }
             
-            /*if (callback.Data == "myCommand1")
-            {
-                Console.WriteLine("кнопка нажата");
-                //await botClient.SendTextMessageAsync(message.Chat, "ffdfdgdgf");
-                //return;
-
-            }*/
-            //Console.WriteLine(update.CallbackQuery.Data);
-
-
+            
+           
         }
        
-        //кнопки формируются тут 
+        //кнопки для стартового выбора действий 
         private static IReplyMarkup buttonTest()
         {
             var ikm = new InlineKeyboardMarkup(new[]
             {
                 new[]
                 {
-                    InlineKeyboardButton.WithCallbackData("создать", "myCommand1"),
-                    InlineKeyboardButton.WithCallbackData("скрыть1", "myCommand2"),
+                    InlineKeyboardButton.WithCallbackData("Турнирная таблица", "tourneyTable"),
+                    InlineKeyboardButton.WithCallbackData("Расписание игр", "gameSchedule"),
                 },
                 new[]
                 {
-                    InlineKeyboardButton.WithCallbackData("скрыть", "myCommand3"),
+                    InlineKeyboardButton.WithCallbackData("Подать заявку", "apply"),
                 },
             });
             return ikm;
         }
 
-        //метод создания кнопики
-
+        
         public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
             Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
         }
+        
         static void Main(string[] args)
         {
+            DBConnection connectsql = new DBConnection();
+            //connectsql.OpenConnect();
+            String sqlstr = "SELECT nameLeague FROM league";
+            connectsql.SqlRead(sqlstr);
+            //блок запуска бота и методов проверки сообщений и обработки ошибок
             Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
             var cts = new CancellationTokenSource();
             var cancellationToken = cts.Token;
